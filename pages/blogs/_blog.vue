de38c1c7496334ac293b45db7b7c095c833365c6
phtchung <phamthanhchung2001@gmail.com>
2023-03-28 21:44:19 +0700

    initial commit

---
+++ pages/blogs/_blog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1> I am a blog page luuuuuuuuuuuuuuuu</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "blogs"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/todoapp.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "todoapp"
+}
+</script>
+
+<style scoped>
+
+</style>
